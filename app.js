1e00402f4fdaf133b09a3b6a23c8e9f69f1e205a
jinX-20 <jyothi.sri@students.iiit.ac.in>
2025-08-23 16:28:57 +0530

    Backend Setup done

--- app.js
+++ app.js
@@ -24,12 +24,48 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => { // socket is the unique information about that user
   console.log("A user connected");
 
-  socket.on("churan", () => {
-    console.log("Churan received from client");
-  })
+  if (!players.white){
+    players.white = socket.id; // every socket has unique id
+    socket.emit("playerRole", "w"); // first player that comes gets white
+  } else if (!players.black) {
+    players.black = socket.id;
+    socket.emit("playerRole", "b");
+  } else{
+    socket.emit("spectatorRole");
+  }
 
   socket.on("disconnect", () => {
-    console.log("User disconnected");
+    if (socket.id) {
+      console.log("A user disconnected");
+      if (players.white === socket.id) {
+        delete players.white;
+      } else if (players.black === socket.id) {
+        delete players.black;
+      }
+    }
+  });
+
+  socket.on("move", (moveMade) => {
+    try{
+      if (chessInstance.turn() === 'w' && socket.id !== players.white) {
+        return;
+      }
+      if (chessInstance.turn() === 'b' && socket.id !== players.black) {
+        return;
+      }
+      const result = chessInstance.move(moveMade);
+      if (result){
+        currentPlayer = chessInstance.turn();
+        io.emit("move", moveMade);
+        io.emit("boardState", chessInstance.fen());
+      } else {
+        console.log("Invalid move : ", moveMade);
+        socket.emit("invalidMove", moveMade);
+      }
+    } catch(err){
+      console.log(err);
+      socket.emit("Invalid move : ", moveMade);
+    }
   });
 });
 
--- public/js/chessgame.js
+++ public/js/chessgame.js
@@ -1,3 +1,13 @@
 const socket = io();
+const chess = new Chess();
+const boardElement = document.querySelector(".chessboard");
 
-socket.emit("churan");
+let draggedPiece = null;
+let sourceSquare = null;
+let playerRole = null;
+
+const renderBoard = () => {};
+
+const handleMove = () => {};
+
+const getPieceUnicode = () => {};
